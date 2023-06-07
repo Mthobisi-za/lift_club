a01c763b3b302bedcd8f880374b21709be9f3761
Mthobisi-za <mthobisimtho333@gmail.com>
2023-06-07 15:22:47 +0200

    done

--- src/HOME/Home.jsx
+++ src/HOME/Home.jsx
@@ -6,6 +6,9 @@ function HOME() {
     <div className="home">
      <div className="left_home_top_tab">
       <img src={require('./assets/LIFT CLUB.png')} alt="" />
+      <h3 className='subtitle'>LOREM IPSUM LOREM IPSUM LOREM IPSUM
+    JKJKKJKJ JKJKKJKJ JKJKKJKJ JKJKKJKJ JKJKKJKJ</h3> <br />
+<button className="btn_dark_blue"> <a href="#about_us">READ MORE</a> </button>
      </div>
      <div className="right_home_top_tab">
       <img src={require('./assets/car_home.png')} alt="" />
